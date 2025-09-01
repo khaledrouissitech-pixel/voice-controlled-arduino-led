aea534a6e6b242981fe846177c9b7f2c9c498da3
Khaled Rouissi <khaledrouissi.tech@gmail.com>
2025-09-01 23:14:17 +0100

    Update led_control.py

--- led_control.py
+++ led_control.py
@@ -6,14 +6,17 @@ import queue
 from vosk import Model, KaldiRecognizer
 
 # --------- Settings ---------
-VOSK_MODEL_DIR = r"C:\Users\KshiKamaru\Desktop\A.R.I.S\models\vosk-en"
-SERIAL_PORT = "COM5"  # change this to your Arduino COM port
+# Path to Vosk model (download from https://alphacephei.com/vosk/models ,i used vosk-model-small-en-us-0.15	)
+VOSK_MODEL_DIR = "models/vosk-en"
+
+# Change COM port to match your Arduino
+SERIAL_PORT = "COM5"
 BAUD_RATE = 9600
 # ----------------------------
 
-# Setup serial connection
+
 arduino = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
-time.sleep(2)  # wait for Arduino to reset
+time.sleep(2)  
 
 # Load Vosk model
 model = Model(VOSK_MODEL_DIR)
@@ -23,7 +26,6 @@ q = queue.Queue()
 
 def callback(indata, frames, time_info, status):
     if rec.AcceptWaveform(bytes(indata)):
-
         result = rec.Result()
         text = json.loads(result)["text"]
         if text:
@@ -32,18 +34,19 @@ def callback(indata, frames, time_info, status):
 
 
 def handle_command(text):
-    if "light" in text or "led on" in text:
+    if "light on" in text or "led on" in text:
         arduino.write(b"LED_ON\n")
         print("💡 Turning LED ON")
-    elif "off" in text or "led off" in text:
+    elif "light off" in text or "led off" in text:
         arduino.write(b"LED_OFF\n")
         print("❌ Turning LED OFF")
 
 
 def main():
     print("🎤 Say 'light on' or 'light off' to control LED")
-    with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype="int16",
-                           channels=1, callback=callback):
+    with sd.RawInputStream(
+        samplerate=16000, blocksize=8000, dtype="int16", channels=1, callback=callback
+    ):
         while True:
             pass
 
